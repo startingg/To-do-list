e4685bc25f34ef34e15d4efc3b297cc623e43e77
이시영 <jltldud@gmail.com>
2023-08-03 15:50:15 +0900

    Initial Commit

---
+++ todolist_test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  todolist_test
+//
+//  Created by t2023-m0064 on 2023/08/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ todolist_testUITests/todolist_testUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  todolist_testUITestsLaunchTests.swift
+//  todolist_testUITests
+//
+//  Created by t2023-m0064 on 2023/08/03.
+//
+
+import XCTest
+
+final class todolist_testUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
